98d62513bce14c73e29b52d34b3b278a68da9bca
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-04-15 09:20:28 +0000

    timestamp function module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var timestamp = require("./timestamp");
 var express = require("express");
 var app = express();
 
@@ -9,8 +10,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
+
+// index of the website, just to explain how does it works
 app.get("/", function(req, res) {
     res.render("index.html");
+});
+
+// Url API
+app.get("/api/:arg", function(req, res) {
+    var param = req.query.tid;
+    res.end(timestamp.outPut(param));
 });
 
 app.listen(app.get('port'), function() {
---
+++ timestamp.js
@@ -0,0 +1,12 @@
+
+function outPut(argTime) {
+
+    var obj = {};
+    obj.unix = null;
+    obj.natural = null;
+    
+    return JSON.stringify(obj);
+    
+}
+
+module.exports.outPut = outPut;
